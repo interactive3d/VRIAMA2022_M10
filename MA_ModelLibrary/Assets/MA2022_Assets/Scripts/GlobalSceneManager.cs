d18d6da9dcdffe5df3ab456f7c50c987ef64f7ef
interactive3d <marcin.kasica@gmail.com>
2022-03-17 13:32:13 +0100

    Before Events

--- MA_ModelLibrary/Assets/MA2022_Assets/Scripts/GlobalSceneManager.cs
+++ MA_ModelLibrary/Assets/MA2022_Assets/Scripts/GlobalSceneManager.cs
@@ -8,6 +8,7 @@ public class GlobalSceneManager : MonoBehaviour
     // create singleton pattern
     public static GlobalSceneManager Instance { get; internal set; }
 
+    #region MainUnityFunctions
     private void Awake()
     {
         if (!Instance)
@@ -15,19 +16,11 @@ public class GlobalSceneManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(this);
         }
-        else if (Instance!=this)
+        else if (Instance != this)
         {
             Destroy(this);
         }
     }
-
-
-
-    public string UserName { get; set; }         // this will store the UserName as the sting
-    public int QualityLevel { get; set; }                    // this will store the Current Quality Level
-    public float VolumeLevel { get; set; }                      // this will store the Volume Level
-    public bool MuteState { get; set; }          // this will store the state of the mute if true the audio will be muted if false will play
-
 
     private void Start()
     {
@@ -37,11 +30,29 @@ public class GlobalSceneManager : MonoBehaviour
         MuteState = false;
     }
 
+    private void Update()
+    {
+  
+    }
+    #endregion
+
+    #region VARIABLES
+
+    public string UserName { get; set; }         // this will store the UserName as the sting
+    public int QualityLevel { get; set; }                    // this will store the Current Quality Level
+    public float VolumeLevel { get; set; }                      // this will store the Volume Level
+    public bool MuteState { get; set; }          // this will store the state of the mute if true the audio will be muted if false will play
+
+    #endregion
+
+    #region HelperFunctions
+
     public void ChangeScene()
     {
         SceneManager.LoadScene(1);
     }
 
+    #endregion
 
     // add functonality to save the preferences to the file (external file) 
 
